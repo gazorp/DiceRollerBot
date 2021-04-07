64aefff290d8a0f933a96f9c8729b95d11547e10
gazorp <mcorefan@gmail.com>
2021-04-08 01:51:23 +0300

    Add messages logging

--- src/main/kotlin/TelegramBot.kt
+++ src/main/kotlin/TelegramBot.kt
@@ -1,15 +1,26 @@
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.Dispatcher
+import com.github.kotlintelegrambot.dispatcher.message
 import handler.Handler
+import org.slf4j.LoggerFactory
 
 class TelegramBot(token: String, handlers: List<Handler>) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     private val bot: Bot = bot {
         this.token = token
 
         dispatch {
-            handlers.forEach { it.handle(this) }
+            listOf(loggingHandler()).plus(handlers).forEach { it.handle(this) }
+        }
+    }
+
+    private fun loggingHandler(): Handler = object : Handler {
+        override fun handle(dispatcher: Dispatcher) = dispatcher.message {
+            logger.info("[${message.chat.username ?: message.chat.firstName}]: ${message.text}")
         }
     }
 
